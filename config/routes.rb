564b1da1ebcba6db26e12a1f8b8345e725d1a369
Bethany Nowviskie <bethany@virginia.edu>
2011-10-04 15:23:16 -0400

    created scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Suggestitron::Application.routes.draw do
+  resources :topics
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111004192047) do
+
+  create_table "topics", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
