110ab4bda30e0260461a8880529d980061575195
Bethany Nowviskie <bethany@virginia.edu>
2011-10-15 22:18:14 -0400

    deleting default rails index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Suggestitron::Application.routes.draw do
+
   resources :votes
-
   resources :topics
-
+  root :to => 'topics#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
