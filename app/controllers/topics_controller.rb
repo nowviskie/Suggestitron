d7525930187cbf6afe1a477722f5c0dacd6b91a8
Bethany Nowviskie <bethany@virginia.edu>
2011-10-15 21:21:02 -0400

    waynebottifying it

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -60,7 +60,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.update_attributes(params[:topic])
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to @topic, notice: 'Suggestion was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
