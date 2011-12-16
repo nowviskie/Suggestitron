d541768e702bf644549a752acf1fd76aa8e5883d
Bethany Nowviskie <bethany@virginia.edu>
2011-12-15 20:17:18 -0500

    pagination & search

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :votes
 
   def self.search(search, page)
-    paginate :per_page => 5, :page => page,
+    paginate :per_page => 25, :page => page,
       :conditions => ['title like ?', "%#{search}%"], :order => 'title'
   end
 
