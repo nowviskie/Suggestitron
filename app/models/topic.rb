505b8ced4bdec8433d302c3f533f7c8002392ba1
Bethany Nowviskie <bethany@virginia.edu>
2011-12-15 20:21:49 -0500

    20 per page is purtier

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :votes
 
   def self.search(search, page)
-    paginate :per_page => 25, :page => page,
+    paginate :per_page => 20, :page => page,
       :conditions => ['title like ?', "%#{search}%"], :order => 'title'
   end
 
